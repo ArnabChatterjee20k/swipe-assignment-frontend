d7aec1b2f33b41e4da7c53da3f37bc6156ef2afc
arnab <arnabchatterjee.ac.2@gmail.com>
2024-04-08 20:25:42 +0530

    fix -- production addition check if items are already present in the store

--- src/redux/products/productsSlice.js
+++ src/redux/products/productsSlice.js
@@ -5,15 +5,38 @@ const productsSlice = createSlice({
   initialState: [],
   reducers: {
     addProduct: (state, action) => {
-      console.log(action.payload);
-      const receivedItems = action.payload.products.map((item) => ({
-        itemId: generateRandomId(),
-        itemName: item.itemName,
-        itemDescription: item.itemDescription,
-        itemPrice: item.itemPrice,
-        itemQuantity: item.itemQuantity,
-        invoices: [action.payload.invoiceID],
-      }));
+      // concatnating current invoiceID to the items if they exists already
+      // another way is their which will be more of hack is that to check if invoices is coming with the items that means it is already present in the items. But it would loosely the validations
+      const addedItemIDs = [];
+      action.payload.products.forEach((cur) => {
+        const idx = state.findIndex((prev) => prev.itemId === cur.itemId);
+        if (idx !== -1) {
+          addedItemIDs.push(cur.itemId);
+          const previousProductState = current(state)[idx];
+          state[idx] = {
+            ...previousProductState,
+            invoices: [
+              ...previousProductState["invoices"],
+              action.payload.invoiceID,
+            ],
+          };
+        }
+      });
+      const receivedItems = action.payload.products
+        .map((item) => {
+          if (!addedItemIDs.includes(item.itemId))
+            return {
+              itemId: generateRandomId(),
+              itemName: item.itemName,
+              itemDescription: item.itemDescription,
+              itemPrice: item.itemPrice,
+              itemQuantity: item.itemQuantity,
+              invoices: [action.payload.invoiceID],
+            };
+          return null;
+        })
+        .filter((item) => item !== null);
+      console.log({ receivedItems });
       state.push(...receivedItems);
     },
     deleteProduct: (state, action) => {
@@ -51,11 +74,11 @@ const productsSlice = createSlice({
             const updatedInvoices = item.invoices.filter(
               (invoice) => parseInt(invoice) !== invoiceId
             );
-            return { ...item, invoices: updatedInvoices }; 
+            return { ...item, invoices: updatedInvoices };
           }
           return item;
         })
-        .filter((item) => item.invoices.length > 0)
+        .filter((item) => item.invoices.length > 0);
     },
   },
 });
